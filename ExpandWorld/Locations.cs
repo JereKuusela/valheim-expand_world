d8abdb71f4da38f05b8afa7c9f319a9924ba3b72
Jere Kuusela <jere.kuusela@iki.fi>
2022-07-01 21:30:23 +0300

    Adds an automatic fail-safe if some locations can't be placed (like Moder altar).

--- ExpandWorld/Locations.cs
+++ ExpandWorld/Locations.cs
@@ -1,24 +1,43 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Emit;
 using HarmonyLib;
 using UnityEngine;
 
 namespace ExpandWorld;
 [HarmonyPatch(typeof(ZoneSystem), nameof(ZoneSystem.GenerateLocations), new[] { typeof(ZoneSystem.ZoneLocation) })]
-public class GuaranteeStart {
-  static void Postfix(ZoneSystem __instance, ZoneSystem.ZoneLocation location) {
-    if (location.m_prefabName == Game.instance.m_StartLocation && __instance.CountNrOfLocation(location) == 0) {
-      ExpandWorld.Log.LogInfo("Forcefully placing start location at the center.");
+public class GuaranteeLocations {
+  static void GuaranteeSpawn(ZoneSystem zs, ZoneSystem.ZoneLocation location) {
+    if (location.m_prefabName == Game.instance.m_StartLocation && Count(zs, location) == 0) {
+      ExpandWorld.Log.LogInfo($"Forcefully placing {location.m_prefabName} location at the center.");
       var locationRadius = Mathf.Max(location.m_exteriorRadius, location.m_interiorRadius);
-      Vector3 randomPointInZone = __instance.GetRandomPointInZone(new Vector2i(0, 0), locationRadius);
-      __instance.RegisterLocation(location, randomPointInZone, false);
+      Vector3 randomPointInZone = zs.GetRandomPointInZone(new Vector2i(0, 0), locationRadius);
+      zs.RegisterLocation(location, randomPointInZone, false);
     }
+  }
+  static int Count(ZoneSystem zs, ZoneSystem.ZoneLocation location) => zs.m_locationInstances.Values.Count(loc => loc.m_location.m_prefabName == location.m_prefabName);
+  static void AltitudeTweak(ZoneSystem zs, ZoneSystem.ZoneLocation location) {
+    if (Count(zs, location) == 0 && (location.m_minAltitude > 0f || location.m_maxAltitude < 500f)) {
+      ExpandWorld.Log.LogInfo($"Lowering altitude requirement for {location.m_prefabName} location.");
+      var minAltitude = location.m_minAltitude;
+      var maxAltitude = location.m_maxAltitude;
+      location.m_minAltitude -= 50f;
+      location.m_maxAltitude += 100f;
+      zs.GenerateLocations(location);
+      location.m_minAltitude = minAltitude;
+      location.m_maxAltitude = maxAltitude;
+    }
+  }
+  static void Finalizer(ZoneSystem __instance, ZoneSystem.ZoneLocation location) {
+    GuaranteeSpawn(__instance, location);
+    AltitudeTweak(__instance, location);
   }
 }
 [HarmonyPatch(typeof(ZoneSystem), nameof(ZoneSystem.GenerateLocations), new[] { typeof(ZoneSystem.ZoneLocation) })]
 public class GenerateLocationsQuantity {
   static void Prefix(ZoneSystem.ZoneLocation location, ref int __state) {
+    if (location.m_prefabName == Game.instance.m_StartLocation) return;
     __state = location.m_quantity;
     location.m_quantity = Mathf.RoundToInt(location.m_quantity * Configuration.LocationsMultiplier);
   }
